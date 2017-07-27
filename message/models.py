6e0f07581b86bb42b7f96f192ffd512aac0d5f47
LoredanaIosub <loredana.iosub28@gmail.com>
2017-07-27 12:28:20 +0300

    Primul

--- message/models.py
+++ message/models.py
@@ -12,14 +12,31 @@ class Message(TimestampModel):
     user = models.ForeignKey(User)
     status = models.TextField(null=False, max_length=140, blank=False)
 
+    def __str__(self):
+        return self.status
+
+    @property
+    def like_users(self):
+        return[l.user for l in self.like_set.all() if l.like]
+
+    @property
+    def dislike_users(self):
+        return[l.user for l in self.like_set.all() if not l.like]
+
 
 class Like(TimestampModel):
     user = models.ForeignKey(User)
-    message = models.ForeignKey(Message)
+    message =models.ForeignKey(Message)
     like = models.BooleanField()
 
 
+
 class Follow(TimestampModel):
+    class Meta:
+        unique_together = [("followed_user", "following_user"),]
     followed_user = models.ForeignKey(User, related_name='followed_by')
     following_user = models.ForeignKey(User, related_name='following')
+
+    def __str__(self):
+        return "User {0} follows user {1}".format(self.following_user.username, self.followed_user.username)
 
---
+++ static/js/likeDislike.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+    $(document).ready(function () {
+        $('.like-message').click(function () {
+            alert("Pressed a like button!");
+            var request = $.ajax({
+                url: "http://127.0.0.1:8000/like/",
+                method: "POST",
+                data: {id : $(this).attr("data-messageid"),
+                    like: 1
+                }
+
+            });
+
+            request.done(function () {
+                alert("SUCCES!")
+        });
+            request.fail(function () {
+                alert("FAILLL!")
+            });
+        });
+
+        $('.dislike-message').click(function () {
+            alert("Pressed a dislike button!");
+            var request = $.ajax({
+                url: "http://127.0.0.1:8000/like/",
+                method: "POST",
+                data: {id : $(this).attr("data-messageid"),
+                    like: 0
+                }
+
+            });
+
+            request.done(function () {
+                alert("SUCCES!")
+        });
+            request.fail(function () {
+                alert("FAILLL!")
+            });
+        });
+    });
+})();
